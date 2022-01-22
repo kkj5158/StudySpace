a6aa20b19a8a28a37922464a8346c7693ad60f25
KJS <jjk5158@naver.com>
2022-01-22 10:23:52 +0900

    "jap 와 db dependency 추가 "

---
+++ src/test/java/com/jojoldu/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.jojoldu.book.springboot.web.domain.posts;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("jojoldu@gmail.com").build());
+
+    }
+}
