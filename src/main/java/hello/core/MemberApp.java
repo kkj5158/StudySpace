e5fdc2a41ffa0982286d4058e942f39d9c86e59e
Kafka <jjk5158@naver.com>
2022-01-28 14:27:28 +0900

    Initial commit

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args){
+        MemberService memberService = new MemberServiceImpl();
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Member findmember = memberService.findMember(1L);
+
+        System.out.println("new member =" + memberA);
+
+        System.out.println("findMember =" + findmember);
+
+    }
+}
