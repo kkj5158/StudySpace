ec816567339a37cfe82a0736650ba215b8b079ba
Kafka <jjk5158@naver.com>
2022-02-17 18:56:40 +0900

    2022/02/17

--- chap06/10.py
+++ chap06/10.py
@@ -1,11 +1,29 @@
 
-n = int(input())
+n, m = map(int, input().split())
 
-array = []
+graph = []
+
 for i in range(n):
-    array.append(int(input()))
+    graph.append(list(map(int, input())))
 
-array = sorted(array, reverse=True)
+def dfs(x,y):
+    if x<= -1 or x>= n or y<= -1 or y>=m :
+        return False
+    
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
 
-for i in array:
-    print(i, end=' ')
+result = 0
+
+for i in range(n):
+    for j in range(m):
+         if dfs(i,j) == True:
+            result +=1 
+
+print(result)
