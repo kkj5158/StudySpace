230b12db7577cae2313eee044c065c52d9a1523b
Kafka <jjk5158@naver.com>
2022-02-20 16:21:43 +0900

    "02/20"

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLOutput;
 
@@ -28,9 +30,14 @@ public class SingletonTest {
         // 참조값이 다른 것을 확인
 
         System.out.println("memberService 1 = " + memberService1);
+
         System.out.println("memberService 2 = " + memberService2);
 
+        //memberSevice1 != memberService2
+
         assertThat(memberService1).isNotSameAs(memberService2);
+
+        // 우
 
 
     }
@@ -56,5 +63,21 @@ public class SingletonTest {
 
         singletonService1.logic();
 
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
