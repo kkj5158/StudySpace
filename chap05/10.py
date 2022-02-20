d3416ebe9ff4b0bd43b9d9378b581cfd460b13f3
Kafka <jjk5158@naver.com>
2022-02-20 12:29:52 +0900

    dfs

--- chap05/10.py
+++ chap05/10.py
@@ -1,29 +1,43 @@
+# n,m 입력받기
+# 그래프 입력받기 
 
-n, m = map(int, input().split())
+
+## 솔루션 보지 말고 반드시 혼자 힘으로 해결하기 
+
 
 graph = []
 
-for i in range(n):
-    graph.append(list(map(int, input())))
+count = 0 
 
-def dfs(x,y):
-    if x<= -1 or x>= n or y<= -1 or y>=m :
+n, m = map(int, input().split())
+
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+def dfs(graph, x, y):
+    if x<0 or x>m or y< 0 or y>n:
         return False
-    
-    if graph[x][y] == 0:
+    elif graph[x][y] == 1:
+         return False
+    else:
         graph[x][y] = 1
-        dfs(x-1, y)
-        dfs(x, y-1)
-        dfs(x+1, y)
-        dfs(x, y+1)
+        dfs(graph, x+1, y)
+        dfs(graph, x-1, y)
+        dfs(graph, x, y-1)
+        dfs(graph, x, y+1)
+        count = count + 1
         return True
-    return False
 
-result = 0
+for x in range(n):
+    for y in range(m):
+        dfs(graph, x, y)
 
-for i in range(n):
-    for j in range(m):
-         if dfs(i,j) == True:
-            result +=1 
 
-print(result)
+print(count)
+    
+# 파이썬 함수 인수 받는법 연습하기 
+
+
+# 재귀함수는 기능 위주로만 작성한다 . 
+# 시작지점 방문한다. -> 0이면 방문처리한다. 1
+
---
+++ chap05/8.py
@@ -0,0 +1,31 @@
+#DFS 예제 
+
+# 주어진 그래프 
+graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7]
+]
+
+visited = [False] * 9
+
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+dfs(graph, 1, visited)
+
+
+# 재귀함수 알고리즘 더 공부하기 
