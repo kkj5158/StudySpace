bb1ea57b6333e638a3849f9ea3d9d26f07469f74
kafka <jjk5158@naver.com>
2022-03-28 19:45:47 +0900

    "2.6 96p"

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,6 +19,9 @@ public interface ItemRepository extends JpaRepository<Item, Long>{
 
  List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
+ @Query("select i from Item i where i.itemDetail like " +
+         "%:itemDetail% order by i.price desc")
+ List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 
 
 }
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -97,5 +97,15 @@ class ItemRepositoryTest {
 
     }
 
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for(Item item: itemList){
+            System.out.println(item.toString());
+        }
+    }
+
 
 }
