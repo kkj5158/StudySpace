2fe51a20ffe121ce5154cf5740aa7a567f593bf5
kafka <jjk5158@naver.com>
2022-03-29 17:38:53 +0900

    "2.6 96p"

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -107,5 +107,18 @@ class ItemRepositoryTest {
         }
     }
 
+    @Test
+    @DisplayName("nativeQuery 속성을 이용한 상품 조회 테스트")
+    public void findByItemDetailByNative(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
+
+        for(Item item: itemList){
+            System.out.println(item.toString());
+        }
+
+
+    }
+
 
 }
