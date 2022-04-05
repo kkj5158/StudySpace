0782d54bae7471d616a13d56f2173b8f3e57fa03
kafka <jjk5158@naver.com>
2022-04-05 19:41:25 +0900

    2022/04/05

--- chap11 - 그리디문제/6-1무지의먹방라이브.py
+++ chap11 - 그리디문제/6-1무지의먹방라이브.py
@@ -1,11 +1,27 @@
 #https://programmers.co.kr/learn/courses/30/lessons/42891
 import heapq
 
+
 def solution(food_times, k):
-    if sum(food_times) <= k:
+    
+    if(sum(food_time)>=k):
         return -1
+    
+    q = []
+    
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i+1))
+        
+sum_value = 0
+previous = 0 
 
+length = len(food_times) # 남은 음식의 갯수 
 
-q = []
-
-for i in range(len())
+while sum_value + ((q[0][[0]] - previous)*length) <= k:
+    now = heapq.heapop(q)[0]
+    sum_value += (now-previous)*length
+    length -= 1
+    previous = now
+    
+result = sorted(q, key = lambda x : x[1])
+return result[(k - sum_value)% length][1]
