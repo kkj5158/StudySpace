9a6a9a3c5bae2db519f2013b591a655d9d900398
kafka <jjk5158@naver.com>
2022-04-05 20:26:36 +0900

    "2.6 96p"

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,11 +3,12 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long>{
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item>{
  // JpaRepository에는 기본적인 CRUD 및 페이징 처리를 위한 메소드가 저장되어있다.
 
  List<Item> findByItemNm(String itemNm);
@@ -27,4 +28,5 @@ public interface ItemRepository extends JpaRepository<Item, Long>{
          "%:itemDetail% order by i.price desc", nativeQuery = true)
  List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 
-}
+
+ }
