4299c5bed30696d5c0def502d740c13afccd6878
kafka <jjk5158@naver.com>
2022-04-22 21:23:10 +0900

    2022/04/22

--- Project1/HelloWorld.cpp
+++ Project1/HelloWorld.cpp
@@ -1,14 +1,34 @@
 #include <mpi.h>
+#include <string.h>
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-	// Initialize the MPI environment
-	MPI_Init(NULL, NULL);
+const int MAX_STRING = 100;
+
+int main(void) {
+	char greeting[MAX_STRING];
+	int comm_sz;
 	int my_rank;
-	// Get the rank of the processint my_rank;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	// Print the message
-	printf("Hello World! My rank is %d\n", my_rank);
-	// Finalize the MPI environment.
+
+	if (my_rank != 0)
+	{
+		sprintf(greeting, "Greetings from process %d of %d !", my_rank, comm_sz);
+		MPI_Send(greeting, strlen(greeting) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+	}
+	else {
+		printf("Greetings from process %d of %d!\n", my_rank, comm_sz);
+
+		for (int q = 1; q < comm_sz; q++) {
+			MPI_Recv(greeting, MAX_STRING, MPI_CHAR, q, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+			printf("%s\n", greeting);
+
+
+		}
+	}
+
 	MPI_Finalize();
+	return 0;
 }
