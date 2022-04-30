37b1405ec74e5bcf01550e0622c658a74648e844
kafka <jjk5158@naver.com>
2022-04-30 16:49:45 +0900

    "complete version "

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,36 +2,51 @@ package hellojpa;
 
 import javax.persistence.*;
 import javax.xml.bind.annotation.XmlInlineBinaryData;
+import java.util.Date;
 
 
 @Entity
-@Table(name="MEMBER")
+@Table(name="MEMBER", uniqueConstraints = {@UniqueConstraint(name = "NAME_AGE_UNIQUE", columnNames = {"NAME", "AGE"})})
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
-    private String id;
+    private Long id;
 
-    @Column(name="NAME")
-    private String name;
+    @Column(name="NAME", nullable = false, length = 10)
+    private String username;
 
     //매핑 정보가 없는 필드
     private Integer age;
 
-    public String getId() {
+    @Enumerated(EnumType.STRING)
+    private  RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private  Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
     public String getUserName() {
-        return name;
+        return username;
     }
 
     public void setUserName(String name) {
-        this.name = name;
+        this.username = name;
     }
 
     public Integer getAge() {
