50eca21cab8b9624134be2ae7740104b1c4e8a2f
kafka <jjk5158@naver.com>
2022-05-03 13:33:05 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ORDER_ITEM")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ITEM_ID")
+    private Item itemid;
+
+
+    @Column(name = "ORDER_ID")
+    private Order orderid;
+
+    private int orderPrice;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Item getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(Item itemid) {
+        this.itemid = itemid;
+    }
+
+    public Order getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(Order orderid) {
+        this.orderid = orderid;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    //Getter Setter
+}
