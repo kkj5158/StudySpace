938f7034acf435a175181d350a63adbe343aa060
kafka <jjk5158@naver.com>
2022-05-06 19:10:35 +0900

    05/06

---
+++ chap03/5수들의합-best.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin=open("5.txt", "rt")
+
+n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+lt = 0
+rt = 1
+tot =a[0]
+cnt = 0
+
+while True:
+    if tot<m:
+        if rt < n :
+            tot += a[rt]
+            rt += 1
+        else:
+            break
+    elif tot ==m :
+        cnt +=1
+        tot -= a[lt]
+        lt += 1
+    else:
+        tot-=a[lt]
+        lt+=1
+
+print(cnt)
+
---
+++ chap03/6격자판최대합.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin=open("6.txt", "rt")
+
+n = int(input())
+
+map = [list(map(int, input().split())) for _ in range(n)]
+
+
+
+for x in map:
+    
+    
+# 각 행의 합 구하기 , 각 열의 합 구하기 , 두 대각선의 합 구하기 
+ 
