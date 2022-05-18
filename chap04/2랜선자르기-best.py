3378526f37a5f25653c542eed74b8202ee82711e
kafka <jjk5158@naver.com>
2022-05-18 11:28:24 +0900

    2022/05/18

--- chap04/2랜선자르기-best.py
+++ chap04/2랜선자르기-best.py
@@ -1,5 +1,50 @@
+
 import sys
 sys.stdin = open("2.txt")
 
-# 이진 탐색을 활용하여서 구현해보기 
+k, n = map(int, input().split())
 
+a = []
+
+for _ in range(k):
+    a.append(int(input()))
+
+#print(a)
+
+#print(802 + 743 + 457 + 539) # 2541
+#print(2541//11) # 231
+
+
+lt = 1 # 1
+
+rt = sum(a)//n # 231 
+
+mid = lt+rt// 2 # 116  
+
+
+while True:
+
+    cnt = 0
+
+    for l in a :
+        cnt += l//mid
+    
+    if cnt == n:
+        print(mid)
+        break
+
+    elif cnt > n:
+        
+
+    
+
+
+
+    
+
+
+
+
+
+    
+    
---
+++ chap04/3뮤직비디오.py
@@ -0,0 +1,8 @@
+import sys
+sys = open("3.txt")
+
+n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+
