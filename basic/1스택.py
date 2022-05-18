5757baab9c6568d221180f27afea9162c111b1b6
kafka <jjk5158@naver.com>
2022-05-18 13:47:02 +0900

    05/18

---
+++ basic/1스택.py
@@ -0,0 +1,19 @@
+import sys
+sys = open("basic/200자료구조/1.txt")
+
+n = int(input())
+
+
+comm = []
+stack = []
+
+# push 명령어는 어떻게 처리할 것인가 
+
+for _ in range(n):
+    n = input().split()
+    comm.append(input())
+
+
+
+# 스택에 집어넣기 . 빼기 명령어들 실행 . 
+
--- basic/200.1.스택.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys = open("basic/200.1.txt")
-
-n = int(input())
-
