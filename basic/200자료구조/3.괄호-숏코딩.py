8d3f080f9af79235375ee50991a97901d1c5e8a8
kafka <jjk5158@naver.com>
2022-05-31 13:42:36 +0900

    2022/05/31

---
+++ basic/200자료구조/3.괄호-숏코딩.py
@@ -0,0 +1,4 @@
+import sys
+sys.stdin = open("basic/200자료구조/3.txt")
+
+exec(int(input())*("print('YNEOS'[''<input()"+".replace('()','')"*25+"::2]);"))
---
+++ basic/200자료구조/4.스택수열.py
@@ -0,0 +1,6 @@
+import sys
+sys.stdin = open("basic/200자료구조/4.txt")
+
+n = int(input())
+
+
