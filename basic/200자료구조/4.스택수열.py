4af3181c67dddfce499db64d59cecec63bf122b3
kafka <jjk5158@naver.com>
2022-06-01 15:10:33 +0900

    2022/06/01

--- basic/200자료구조/4.스택수열.py
+++ basic/200자료구조/4.스택수열.py
@@ -3,4 +3,29 @@ sys.stdin = open("basic/200자료구조/4.txt")
 
 n = int(input())
 
+ns = []
+
+for _ in range(n):
+    ns.append(int(input()))
+
+#print(ns)
+
+st = []
+
+top = 0
+
+for n in ns:
+    if(top == n):
+        st.pop()
+        print('-')
+        top -= 1
+    else:
+        while top<n:
+            top += 1
+            st.append(top)
+            print('+')
+        top -= 1    
+        st.pop()
+        print('-')
+
 
