ee826c08983b945e0dff73c44a817e22342819eb
kafka <jjk5158@naver.com>
2022-07-09 16:04:41 +0900

    "2022/07/07"
    JPA 실전 1 42p 상품 도메인 개발 부터

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -6,21 +6,30 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
 
     @PersistenceContext
-    EntityManager em;
+    private EntityManager em;
 
-    public Long save(Member member){
+    public void save(Member member){
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id){
+    public Member findone(Long id){
         return em.find(Member.class, id);
     }
 
 
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    // 이름이 동일한 member들은 모두 리스트로 반환한다.
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class).setParameter("name", name).getResultList();
+
+    }
 }
