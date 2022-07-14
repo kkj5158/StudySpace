6e3059a91fcff61ad41c93e85b794fe135e24805
kafka <jjk5158@naver.com>
2022-07-14 18:26:04 +0900

    2022/07/14

--- chap05/1가장큰수.py
+++ chap05/1가장큰수.py
@@ -5,25 +5,32 @@ num, n = map(int, input().split())
 
 num = list(str(num))
 
-
-# 5 2 7 6 8 2 3 
-
+# 5276823 # 7823 
 
 st = []
 
+cnt=0
+
 st.append(num[0])
 
+tmp = num[0]
+
 for a in num[1:]:
-
-    # 숫자를 교체하지 않는 경우 
-
-    b = st.pop()
-
-    # 숫자를 교체함으로 삭제하는 경우 
-    if b <= a and n!=0:
+    if(n==0):
         st.append(a)
-        n = n-1
-    # 숫자 교체가 필요없는 경우 (기존 숫자가 크다)
     else:
-        st.append(b)
-        
+        if(tmp < a):
+            n = n-len(st)
+            st.clear()
+            st.append(a)
+            tmp=a
+        else:
+            st.append(a)
+            st.sort()
+            tmp=st[-1]
+
+
+print(st)
+
+
+# 해결방법 고민해보기 ! 
