4d841bce7ed472c7467ac0158a312f864144313b
kafka <jjk5158@naver.com>
2022-08-08 10:12:56 +0900

    2022/08/08

---
+++ chap05/3.후위표기식.py
@@ -0,0 +1,31 @@
+
+import sys
+sys.stdin=open("chap05/3.txt", "r")
+a=input()
+stack=[]
+res=''
+for x in a:
+    if x.isdecimal():
+        res+=x
+    else:
+        if x=='(':
+            stack.append(x)
+        elif x=='*' or x=='/':
+            while stack and (stack[-1]=='*' or stack[-1]=='/'):
+                res+=stack.pop()
+            stack.append(x)
+        elif x=='+' or x=='-':
+            while stack and stack[-1]!='(':
+                res+=stack.pop()
+            stack.append(x)
+        elif x==')':
+            while stack and stack[-1]!='(':
+                res+=stack.pop()
+            stack.pop()
+while stack:
+    res+=stack.pop()
+print(res)
+
+
+
+
