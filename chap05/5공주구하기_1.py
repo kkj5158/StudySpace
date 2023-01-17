c1f9d7e6dfab4da0a8e96483912f2b80c797c00c
kafka <jjk5158@naver.com>
2023-01-17 10:12:24 +0900

    2023/01/17

--- chap05/5공주구하기_1.py
+++ chap05/5공주구하기_1.py
@@ -1,4 +1,6 @@
 from collections import deque
+import sys
+sys.stdin=open("chap05/5.txt", "r")
 
 num , cnt = map(int, input().split())
 
@@ -26,3 +28,16 @@ while 1:
 result = que.popleft()
 
 print(result)
+# 1 2 3 4 5 6 7 8
+
+# 
+
+# cnt = 0 실행  
+
+# 큐의 길이기 1이 될때까지 다음의 과정 반복
+
+# popleft -> append cnt ++ 
+
+#  cnt가 3이면은 popleft만 실행 
+
+
